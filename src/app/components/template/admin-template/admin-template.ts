f558eac867a9c3ef2a813310e7883931437d1320
AndrebarreiraCastro <andreghoul18@gmail.com>
2025-11-05 21:34:05 -0300

    template de admin  + sidebar

--- src/app/components/template/admin-template/admin-template.ts
+++ src/app/components/template/admin-template/admin-template.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-admin-template',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './admin-template.html',
   styleUrl: './admin-template.css'
 })
---
+++ src/app/components/template/admin-template/sidebar/sidebar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatDrawer, MatDrawerContent, MatDrawerContainer } from '@angular/material/sidenav';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { MatToolbar } from "@angular/material/toolbar";
+import { SidebarService } from '../../../../services/sidebar-service';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [MatNavList, MatListItem, RouterLink, MatDrawerContainer, MatDrawer, MatToolbar, MatDrawerContent, RouterOutlet],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent implements OnInit {
+
+  @ViewChild("drawer") public drawer!: MatDrawer;
+
+  constructor(private sidebarService: SidebarService) {
+
+  }
+
+  ngOnInit(): void {
+    this.sidebarService.sideNavToggleSubject.subscribe(
+      () => { 
+        this.drawer?.toggle();
+      }
+    )
+  }
+
+}
